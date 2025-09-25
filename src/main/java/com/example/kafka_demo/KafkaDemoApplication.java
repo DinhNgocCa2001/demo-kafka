208f791517d15f1df729c6c5813c92c25be99115
Cadn <114199652+DinhNgocCa2001@users.noreply.github.com>
2025-09-25 16:22:54 +0700

    update: fix sona

--- src/main/java/com/example/kafka_demo/KafkaDemoApplication.java
+++ src/main/java/com/example/kafka_demo/KafkaDemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.kafka_demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
+@Slf4j
 public class KafkaDemoApplication {
 
 	public static void main(String[] args) {
@@ -13,11 +15,11 @@ public class KafkaDemoApplication {
 
 	@KafkaListener(topics = "my-topic", groupId = "group1")
 	public void listenGroupFoo(String message) {
-		System.out.println("Consumer1: Received Message in group foo" + message);
+		log.info("Consumer1: Received Message in group foo" + message);
 	}
 
 	@KafkaListener(topics = "my-topic", groupId = "group1")
 	public void listenGroupFoox1(String message) {
-		System.out.println("Consumer2: Received Message in group foo: " + message);
+		log.info("Consumer2: Received Message in group foo: " + message);
 	}
 }
